3a248e3f97a5ac81404f6bb66e9d0d3154c76900
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-07-11 10:59:36 +0530

    name functionality

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function print(){
-	var label2 = document.getElementById("name2");
-	var label1 = document.getElementById("name1");
-	label1.innerHTML = label2.value;
+var label2 = document.getElementById("name2");
+var label1 = document.getElementById("name1");
+
+function print() {
+  window.location.href = "/?name=" + label2.value;
 }
+
+var urlParams = new URLSearchParams(window.location.search);
+label1.innerHTML = urlParams.get('name');
